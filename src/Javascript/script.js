1b0d1c23c5a5c8e5d93a722f402b1908fa1ee499
Elisa Cesario <ecnhamuanzo25@gmail.com>
2026-05-25 10:14:44 +0200

    final

--- src/Javascript/script.js
+++ src/Javascript/script.js
@@ -28,8 +28,29 @@ $(document).ready(function(){
                 return false;
             }
         });
-
+        
+        navItems.removeClass('active')
        $(navItems[activeSectionIndex]).addClass('active');
         
+     });
+     ScrollReveal().reveal('#cta',{
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+     });
+      ScrollReveal().reveal('.dish',{
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+     });
+      ScrollReveal().reveal('#testimonial_chef',{
+        origin: 'left',
+        duration: 1000,
+        distance: '20%'
+     });
+      ScrollReveal().reveal('.feedback',{
+        origin: 'right',
+        duration: 1000,
+        distance: '20%'
      });
 })   
